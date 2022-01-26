420e8751af0ba3154cefde4a63e0dc821e562e32
Abel Murgas <abel.murgas@mibus.com.pa>
2022-01-26 15:16:48 -0500

    created test of list function

---
+++ source/FunctionReading/test/test_list_function.py
@@ -0,0 +1,49 @@
+from source.FunctionReading.function_reading import AlgebraicFunctionReading as afr
+
+def test_error_symbol():
+    try:
+        afr(list_function=[2,2.5,-2,'/'])
+        fail = False
+    except:
+        fail = True
+    assert fail == True
+    
+def test_error_str_number():
+    try:
+        afr(list_function=['2.2','2'])
+        fail = False
+    except:
+        fail = True
+    assert fail == True
+    
+def test_error_list_max():
+    try: # no pass
+        afr(list_function=[2,-1,2,4,5,0,10])
+        fail = False
+    except:
+        fail = True
+    assert fail == True
+    
+def test_error_list_min():
+    try: # no pass
+        afr(list_function=[])
+        fail = False
+    except:
+        fail = True
+    assert fail == True
+    
+def test_pass_list_min():
+    try:
+        afr(list_function=[2])
+        fail = False
+    except:
+        fail = True
+    assert fail == False
+        
+def test_pass_list_max():
+    try:
+        afr(list_function=[2,-1,2,4,5,0])
+        fail = False
+    except:
+        fail = True
+    assert fail == False
